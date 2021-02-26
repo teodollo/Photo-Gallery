7249ec3dd8329d7aa57960599f5031decf7efc33
teodollo <teodollo@github.com>
2021-02-26 10:44:50 +0100

    Fixed search to include captions

--- js/search.js
+++ js/search.js
@@ -1,14 +1,17 @@
 function search_gallery() { 
     let input = document.getElementById('searchbar').value 
-    input=input.toLowerCase(); 
-    let x = document.getElementsByClassName('img'); 
+    input= input.toLowerCase(); 
+    let x = document.getElementsByTagName('A'); 
       
+
     for (i = 0; i < x.length; i++) {  
-        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
-            x[i].style.display="none"; 
+        if (x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="block"; 
+            
         } 
         else { 
-            x[i].style.display="list-item";                  
+            x[i].style.display="none";                  
         } 
     } 
 } 
+console.log(document.getElementsByTagName('A')[11]);
